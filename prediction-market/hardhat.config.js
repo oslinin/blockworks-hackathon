351a8fffcd8a6a3716671b47780c737e36594e36
oslinin <oleg.slinin@gmail.com>
2025-07-10 01:44:43 +0000

    vp

--- prediction-market/hardhat.config.js
+++ prediction-market/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 };
